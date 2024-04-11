badfb3b03c9c02ae05069cfbc7f71ea70e926035
meng fanlei <lei.lei.fan.meng@gmail.com>
2024-04-11 17:58:24 +0800

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,13 +63,10 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/Programing', label: 'Programing', position: 'left'},
+          {to: '/Algo', label: 'Algo', position: 'left'},
+          {to: '/others', label: 'others', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',

b09520bfba1eb25177cd0f5961ee66e71f5db039
meng fanlei <lei.lei.fan.meng@gmail.com>
2024-04-11 17:30:56 +0800

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Lei\'s blog and personal knowledge collection',
+  tagline: '....',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'lei\'s blog', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Lei\'s blog',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -87,22 +87,34 @@ const config = {
                 label: 'Tutorial',
                 to: '/docs/intro',
               },
+              {
+                label: 'Learn Programing Language',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Learn Image processing',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Learn Deep Learning',
+                to: '/docs/intro',
+              },
+              {
+                label: 'Paper Reading',
+                to: '/docs/intro',
+              },
             ],
           },
           {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
+                label: 'Github Home Page',
                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: 'Email',
+                href: 'lei.lei.fan.meng@gmail.com',
               },
             ],
           },
@@ -120,7 +132,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()}  Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
--- src/pages/markdown-page.md
+++ src/pages/markdown-page.md
@@ -6,4 +6,3 @@ title: Markdown page example
 
 You don't need React to write simple standalone pages.
 
-haha
